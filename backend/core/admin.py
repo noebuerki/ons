4ee8cab1aa049dc53ed5f6da8692c143a48351e9
Joel-Luca <joel.christinet@gmx.ch>
2024-03-13 12:02:09 +0100

    Init core app

---
+++ backend/core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from core import models
+
+
+@admin.register(models.Name)
+class NameAdmin(admin.ModelAdmin):
+    search_fields = ["id", "name", "user"]
+    list_display = ["id", "name", "gender", "user"]
+    list_filter = ["gender"]
---
+++ backend/core/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from core import models
+
+
+class NameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Name
+        fields = ["id", "name", "gender"]
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context["request"].user
+        return super().create(validated_data)
