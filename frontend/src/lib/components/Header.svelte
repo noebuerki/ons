5f6a2475a0ccc96d0648c1830b0cba30a048033f
Joel-Luca <joel.christinet@gmx.ch>
2024-05-01 11:38:25 +0200

    Use app logo in navbar

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import { NavBrand, NavHamburger, NavLi, NavUl, Navbar } from "flowbite-svelte";
+    import logo from '$lib/assets/logo.svg';
 </script>
 <Navbar let:NavContainer color="primary">
     <NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
         <NavBrand href="/">
+            <img alt="NJL-Logo" class="h-6 mx-2" src="{logo}" />
             <span class="self-center whitespace-nowrap text-xl font-semibold">Online Name Server</span>
         </NavBrand>
         <NavHamburger />
