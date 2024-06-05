30def95395c6c6e97f3203829225701e98ccf7fb
Ynefota <lars4sprenger@gmail.com>
2024-06-05 12:03:22 +0200

    CRUD Operations

--- frontend/src/lib/components/NameList.svelte
+++ frontend/src/lib/components/NameList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Li, List, P, Hr, Heading, Card, Button } from "flowbite-svelte";
+    import { Li, List, P, Hr, Heading, Card, SpeedDial, Listgroup, ListgroupItem } from "flowbite-svelte";
 	import { createEventDispatcher } from "svelte";
 	import type { Name } from "../../models/name";
 
@@ -11,6 +11,10 @@
     function remove(name: Name) {
         dispatch('remove', name);
     }
+
+    function edit(name: Name) {
+        dispatch('edit', name);
+    }
 </script>
 <Card size="none">
     <Heading tag="h5" class="text-center">{title}</Heading>
@@ -20,8 +24,18 @@
         {#each list as item}
             <Li class="py-1 font-normal">
                 <Card padding="none">
-                    <div class="flex place-content-between items-center p-1">
-                        <P>{item.name}</P><Button on:click={() => remove(item)} size="xs">remove</Button>
+                    <div class="flex place-content-between items-center py-1 px-2">
+                        <P>{item.name}</P>
+                        <SpeedDial tooltip="none" placement="top-end">
+                            <Listgroup class="divide-none" active>
+                                <ListgroupItem class="flex gap-2 md:px-5" on:click={() => remove(item)}>
+                                    Delete
+                                </ListgroupItem>
+                                <ListgroupItem class="flex gap-2 md:px-5" on:click={() => edit(item)}>
+                                    Edit
+                                </ListgroupItem>
+                            </Listgroup>
+                        </SpeedDial>
                     </div>
                 </Card>
             </Li>
--- frontend/src/routes/Dashboard.svelte
+++ frontend/src/routes/Dashboard.svelte
@@ -4,11 +4,15 @@
 	import { Gender } from "../models/gender";
 	import { Modal, Button} from "flowbite-svelte";
 	import type { Name } from "../models/name";
-	import { createName, deleteName } from "$lib/name-api";
+	import { createName, deleteName, updateName } from "$lib/name-api";
+	import EditName from "$lib/components/EditName.svelte";
 
     export let namelist: Array<Name> = [];
     
     let modalOpen = false;
+    let editModalOpen = false;
+
+    let currentEditName: Name | null = null;
     
     async function addName({detail}: any) {
         if (!detail) return;
@@ -21,6 +25,7 @@
             namelist = [...namelist, res.data];
         }
     }
+
     async function removeName(name: Name) {
         const res = await deleteName(name.id);
 
@@ -30,26 +35,70 @@
             namelist = namelist.filter(n => n.id != name.id);
         }
     }
+
+    async function editName(name: Name) {
+        editModalOpen = true;
+        currentEditName = name;
+    }
+
+    async function saveName(name: Partial<Name>) {
+        editModalOpen = false;
+
+        try {
+            if (!currentEditName) {
+                return;
+            }
+            
+            await updateName(currentEditName.id, name);
+            if (name.name) currentEditName.name = name.name;
+            if (name.gender) currentEditName.gender = name.gender;
+            
+            namelist = [...namelist.filter((element) => element.id !== currentEditName?.id), currentEditName];
+        } catch {
+            alert('Failed to update name');
+        }
+
+        currentEditName = null;
+    }
 </script>
 <div class="grid grid-cols-3 gap-3">
     <!--male list-->
     <div class="mb-auto">
-        <NameList title="👨 - Male" on:remove={({detail}) => removeName(detail)} list={namelist.filter(e => e.gender == Gender.MALE)}></NameList>
+        <NameList
+            title="👨 - Male"
+            list={namelist.filter(e => e.gender == Gender.MALE)}
+            on:remove={({detail}) => removeName(detail)}
+            on:edit={({detail}) => editName(detail)}>
+        </NameList>
     </div>
 
     <!--unisex list-->
     <div class="mb-auto">
-        <NameList title="🧒 - Unisex" on:remove={({detail}) => removeName(detail)} list={namelist.filter(e => e.gender == Gender.UNISEX)}></NameList>
+        <NameList
+            title="🧒 - Unisex"
+            list={namelist.filter(e => e.gender == Gender.UNISEX)}
+            on:remove={({detail}) => removeName(detail)}
+            on:edit={({detail}) => editName(detail)}>
+        </NameList>
     </div>
 
     <!--female list-->
     <div class="mb-auto">
-        <NameList title="👩 - Female" on:remove={({detail}) => removeName(detail)} list={namelist.filter(e => e.gender == Gender.FEMALE)}></NameList>
+        <NameList
+            title="👩 - Female"
+            list={namelist.filter(e => e.gender == Gender.FEMALE)}
+            on:remove={({detail}) => removeName(detail)}
+            on:edit={({detail}) => editName(detail)}>
+        </NameList>
     </div>
 </div>
 <Modal bind:open={modalOpen} title="Add name">
     <AddName on:submit={addName} names={namelist.map(n => n.name)}></AddName>
 </Modal>
+<Modal bind:open={editModalOpen} title="Edit name">
+    <EditName on:submit={({detail}) => saveName(detail)} name={currentEditName} names={namelist.map(n => n.name)}></EditName>
+</Modal>
+
 <Button class="absolute end-6 bottom-6 aspect-square" on:click={() => (modalOpen = true)} pill={true}>
     +
 </Button>
