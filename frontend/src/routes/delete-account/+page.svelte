888aaa2435a5d72c76819bbbd9c4c11b05522e2e
Ynefota <lars4sprenger@gmail.com>
2024-06-05 10:54:52 +0200

    Improve delete account page

--- frontend/src/routes/delete-account/+page.svelte
+++ frontend/src/routes/delete-account/+page.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-	import { getContext, onMount } from "svelte";
-	import type { Writable } from "svelte/store";
+	import { getContext } from "svelte";
+	import { get, type Writable } from "svelte/store";
 	import type { User } from "../../models/user";
 	import { goto } from "$app/navigation";
 	import { deleteAccount } from "$lib/login-api";
+	import { Button, P } from "flowbite-svelte";
 
     const user: Writable<User> = getContext('user');
-        
-    onMount(async() => {
-        if ($user.id) {
-            deleteAccount($user.id)
-                .then(() => {
-                    user.set({ id: null, email: null, username: null, loggedIn: false})
-                    goto('/');
-                });
+
+    async function onDeleteClicked() {
+        const userId = get(user).id;
+        if (userId) {
+            await deleteAccount(userId);
+            user.set({ id: null, email: null, username: null, loggedIn: false});
+            goto('/');
         }
-    });
+    }
 </script>
+<div class="flex flex-col place-content-center  gap-2">
+    <P>Are you sure you want to delete your account</P>
+
+    <Button color="red" on:click={onDeleteClicked}>Delete my account</Button>
+</div>
